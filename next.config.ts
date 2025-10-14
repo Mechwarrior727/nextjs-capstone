3e87c9bd7ad2d06734a4d6acf5f58f948e5f1ec6
epicexcelsior <epicexcelsior@gmail.com>
2025-10-14 13:34:23 -0800

    pls work vercel pls

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 // next.config.ts
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    // This is the same as the default, but resolves the warning
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
